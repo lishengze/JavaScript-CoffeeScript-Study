63bacf51b7a98be1516cf2fb9cf2d01fece22c63
lishengze <lishengz.cool@hotmail.com>
2016-03-15 17:55:38 +0800

    一些测试

--- creat-object.js
+++ creat-object.js
@@ -37,22 +37,85 @@
 // 实例与原型间松散的连接，
 // 用字面对象给原型赋值，会改变原型默认的属性断裂其与实例的连接。
 
-function person_c() {
-
-}
-
-person_c.prototype.sayHello = function () {
-  console.log("Hello person_c!");
-}
-
-var ins_person_c1 = new person_c();
-ins_person_c1.sayHello();
-
-person_c.prototype = {
-  sayHi : function() {
-    console.log("Hi person_c!");
+function testLiteralObject () {
+  function person_c() {}
+  
+  person_c.prototype.sayHello = function () {
+    console.log("Hello person_c!");
   }
+  
+  var ins_person_c1 = new person_c();
+  ins_person_c1.sayHello();
+  
+  person_c.prototype = {
+    sayHi : function() {
+      console.log("Hi person_c!");
+    }
+  }
+  
+  ins_person_c1.sayHi(); // error,  .sayHi is undefined!
+  ins_person_c1.sayHello();
 }
 
-ins_person_c1.sayHi(); // error,  .sayHi is undefined!
-ins_person_c1.sayHello();
+/*
+测试: 函数内部用this指针给函数对象创建属性与在函数外部创建属性对函数的影响
+结果: 
+*/
+function teseInnerThisAndOutterFunc() {
+  
+  function test1() {
+    this.life = 'pain!';
+  }
+  
+  function test2() {}
+  test2.life = 'fight!'
+  
+  console.log ('test1:');
+  console.log (test1);
+  
+  console.log ('\ntest2:');
+  console.log (test2);  
+  
+    var EventEmitter = require('events').EventEmitter;
+
+    var path = require('path');
+
+    function ClientMain1() {}
+
+    ClientMain1.emitter = new EventEmitter;
+
+    ClientMain1.childFilePath = path.join(__dirname, 'client-child-complete-b.js');
+
+
+    console.log('\nClientMain1:');
+    console.log(ClientMain1);  
+    
+    var ClientMain1Object = new ClientMain1();
+    console.log('\nClientMain1Object:');
+    console.log(ClientMain1Object);
+    
+    function ClientMain2() {
+   
+      this.emitter = new EventEmitter;
+  
+      this.childFilePath = path.join(__dirname, 'client-child-complete-b.js');         
+    }
+
+    console.log('\nClientMain2:');
+    console.log(ClientMain2); 
+    
+    var ClientMain2Object = new ClientMain2();
+    console.log('\nClientMain2Object:');
+    console.log(ClientMain2Object);
+    
+    // console.log('\nthis: ');
+    // console.log(this.childFilePath);
+     
+}
+
+(function(){
+ 
+ teseInnerThisAndOutterFunc();
+  
+})();
+
